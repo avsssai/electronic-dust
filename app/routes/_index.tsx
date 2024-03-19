9986455c613c4193a39bcb18e9d5d92489b3bb86
shiva addanki <avsssai@gmail.com>
2024-03-19 17:50:51 +0530

    added animations

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,8 @@ import type { MetaFunction } from "@remix-run/node";
 import { Copyright } from "lucide-react";
 import { Slideshow } from "~/components/Slideshow";
 import { Flowers, Landscapes, imageData2 } from "~/components/imageData";
-import { motion } from "framer-motion";
+import { Variants, motion, useInView } from "framer-motion";
+import { createRef, useRef } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,11 +12,27 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const FadeUpVariants: Variants = {
+  initial: {
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
 export default function Index() {
+  // const targetRef = useRef<HTMLDivElement>(null);
+  // const inView = useInView
   return (
     <div>
-      <section className="bg-mainOrange h-screen relative isolate flex items-center">
-        <div className="w-full aspect-video relative flex items-center justify-center max-h-screen">
+      <motion.section className="bg-mainOrange h-screen relative isolate flex items-center">
+        <motion.div className="w-full aspect-video relative flex items-center justify-center max-h-screen">
           {/* <img
             src="/cameraGif.gif"
             alt="Camera gif"
@@ -24,11 +41,17 @@ export default function Index() {
           <h1 className="text-[10vw] text-center font-display z-30 absolute text-black">
             Electronic Dust
           </h1>
-        </div>
-      </section>
+        </motion.div>
+      </motion.section>
       <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 min-h-screen">
         <div className="py-5 px-2 flex flex-col items-center justify-center bg-peach">
-          <div className="py-4 px-10">
+          <motion.div
+            className="py-4 px-10"
+            initial="initial"
+            whileInView="animate"
+            variants={FadeUpVariants}
+            viewport={{ margin: "-300px", once: true }}
+          >
             <img
               src="/photos/image12.jpeg"
               alt="windowsXP"
@@ -37,10 +60,16 @@ export default function Index() {
             <p className="text-center font-label mt-4 text-lg">
               The Indian Windows XP
             </p>
-          </div>
+          </motion.div>
         </div>
         <div className="py-5 px-2 flex flex-col items-center justify-center bg-pastelWhite">
-          <div className="bg-white py-4 px-10 shadow-lg">
+          <motion.div
+            className="bg-white py-4 px-10 shadow-lg"
+            initial="initial"
+            whileInView="animate"
+            variants={FadeUpVariants}
+            viewport={{ margin: "-300px", once: true }}
+          >
             <img
               src="/photos/image5.jpg"
               alt="image1"
@@ -49,12 +78,18 @@ export default function Index() {
             <p className="text-center font-label  mt-4 text-lg">
               Transcendence
             </p>
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="grid grid-cols-1 grid-rows-1 min-h-screen">
         <div className="py-5 px-2 flex flex-col items-center bg-pastelGreen justify-center">
-          <div className="bg-white p-2 md:p-4 shadow-lg">
+          <motion.div
+            className="bg-white p-2 md:p-4 shadow-lg"
+            initial="initial"
+            whileInView="animate"
+            variants={FadeUpVariants}
+            viewport={{ margin: "-300px", once: true }}
+          >
             <img
               src="/photos/image21.jpg"
               alt="image1"
@@ -63,22 +98,34 @@ export default function Index() {
             {/* <p className="text-center font-label  mt-4 text-lg">
               Transcendence
             </p> */}
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 min-h-screen">
         <div className="py-5 px-2 flex flex-col items-center justify-center bg-pear">
-          <div className="py-4 px-10">
+          <motion.div
+            className="py-4 px-10"
+            initial="initial"
+            whileInView="animate"
+            variants={FadeUpVariants}
+            viewport={{ margin: "-300px", once: true }}
+          >
             <img
               src="/photos/image11.jpeg"
               alt="A dog staring at the distance."
               className="object-contain max-h-[calc(100vh-72px)]"
             />
             <p className="text-center font-label mt-4 text-lg">The dog in us</p>
-          </div>
+          </motion.div>
         </div>
         <div className="py-5 px-2 flex flex-col items-center justify-center bg-neutral-200">
-          <div className="bg-white py-4 px-10 shadow-lg">
+          <motion.div
+            className="bg-white py-4 px-10 shadow-lg"
+            initial="initial"
+            whileInView="animate"
+            variants={FadeUpVariants}
+            viewport={{ margin: "-300px", once: true }}
+          >
             <img
               src="/photos/image13.jpeg"
               alt="A photo of hyderabad around the durgamcheruvu"
@@ -87,17 +134,29 @@ export default function Index() {
             <p className="text-center font-label  mt-4 text-lg">
               'Bhagya'nagaram
             </p>
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="grid grid-cols-1 grid-rows-1 min-h-screen">
-        <div className="py-5 px-2 flex flex-col items-center bg-slate-800 justify-center relative overflow-hidden">
+        <motion.div
+          className="py-5 px-2 flex flex-col items-center bg-slate-800 justify-center relative overflow-hidden"
+          initial="initial"
+          whileInView="animate"
+          variants={FadeUpVariants}
+          viewport={{ margin: "-300px", once: true }}
+        >
           <Slideshow imageData={Flowers} />
-        </div>
+        </motion.div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 min-h-screen">
         <div className="py-5 px-2 flex flex-col items-center justify-center bg-white">
-          <div className="py-4 px-10">
+          <motion.div
+            className="py-4 px-10"
+            initial="initial"
+            whileInView="animate"
+            variants={FadeUpVariants}
+            viewport={{ margin: "-300px", once: true }}
+          >
             <img
               src="/photos/image10.jpg"
               alt="Church"
@@ -106,22 +165,34 @@ export default function Index() {
             <p className="text-center font-label mt-4 text-lg">
               The Lord's House
             </p>
-          </div>
+          </motion.div>
         </div>
         <div className="py-5 px-2 flex flex-col items-center bg-black justify-center">
-          <div className="bg-white py-4 px-10 shadow-lg">
+          <motion.div
+            className="bg-white py-4 px-10 shadow-lg"
+            initial="initial"
+            whileInView="animate"
+            variants={FadeUpVariants}
+            viewport={{ margin: "-300px", once: true }}
+          >
             <img
               src="/photos/image15.jpg"
               alt="clouds"
               className="object-contain max-h-[calc(100vh-72px)]"
             />
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="grid grid-cols-1 grid-rows-1 min-h-screen">
-        <div className="py-5 px-2 flex flex-col items-center bg-landscape justify-center relative overflow-hidden">
+        <motion.div
+          initial="initial"
+          whileInView="animate"
+          variants={FadeUpVariants}
+          viewport={{ margin: "-300px", once: true }}
+          className="py-5 px-2 flex flex-col items-center bg-landscape justify-center relative overflow-hidden"
+        >
           <Slideshow imageData={Landscapes} />
-        </div>
+        </motion.div>
       </div>
       <footer className="bg-black text-white px-15 py-10 items-center flex flex-col justify-center">
         <h1 className="flex gap-2 text-2xl items-center">
